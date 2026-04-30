3fff2c573db24cd7a0437f720c64a5a047cb90f7
raiiga <roman225785@gmail.com>
2026-04-30 15:55:22 +0300

    main: removed redundant enum and field

--- src/main/java/com/github/raiiga/testapp/dto/AnalyticsFilter.java
+++ src/main/java/com/github/raiiga/testapp/dto/AnalyticsFilter.java
@@ -1,14 +1,11 @@
 package com.github.raiiga.testapp.dto;
 
-import com.github.raiiga.testapp.dto.type.AnalyticsOrder;
-
 import java.time.LocalDate;
 
 public record AnalyticsFilter(
         String role,
         String position,
         LocalDate since,
-        LocalDate until,
-        AnalyticsOrder order
+        LocalDate until
 ) {
 }
--- src/main/java/com/github/raiiga/testapp/dto/type/AnalyticsOrder.java
+++
@@ -1,6 +0,0 @@
-package com.github.raiiga.testapp.dto.type;
-
-public enum AnalyticsOrder {
-    MOST,
-    LEAST
-}
